06c3dc24f729cdb4af1518ff463b1c836f1c03b3
TUTTIK-FRUTTIK <76999364+TUTTIK-FRUTTIK@users.noreply.github.com>
2026-07-31 01:26:59 +0500

    Fix

--- Build/WebglGame.loader.js
+++ Build/WebglGame.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 56731657,
+    wasmFileSize: 56727273,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
